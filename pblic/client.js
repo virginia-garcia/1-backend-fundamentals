ae72c2c475b675d189b1da7226941d339f19d9c8
Virginia Garcia <85966206+virginia-garcia@users.noreply.github.com>
2022-02-17 18:25:28 -0300

    Create client.js

---
+++ pblic/client.js
@@ -0,0 +1,9 @@
+$(function() {
+  $.get("/users", function(users) {
+    users.forEach(function(user) {
+      $("<li></li>")
+        .text(user)
+        .appendTo("ul#users");
+    });
+  });
+});
